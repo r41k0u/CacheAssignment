9a8032d69e3ff5c1d77304d92665823edb357894
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2024-02-22 07:59:09 +0530

    Make Abstractions

--- CacheAssn.cpp
+++ CacheAssn.cpp
@@ -1,6 +1,18 @@
 ﻿#include "CacheAssn.h"
 
 template<typename K, typename V>
+AbstractCache<K, V>::AbstractCache(size_t cache_size) : max_cache_size(cache_size) {
+	if (cache_size < 8) {
+		std::cout << "Too low cache size. Setting it to 8 elements\n";
+		max_cache_size = 8;
+	}
+	if (cache_size > 65536) {
+		std::cout << "Too large cache size. Setting it to 65536 elements\n";
+		max_cache_size = 65536;
+	}
+}
+
+template<typename K, typename V>
 void FIFOCache<K, V>::insert(const K& key, const V& value) {
 	if (cache_map.size() == max_cache_size) {
 		cache_map.erase(fifo_map.front());
@@ -30,21 +42,8 @@ FIFOCache<K, V>::~FIFOCache() {
 int main()
 {
 	// Tests for the FIFOCache class
-	FIFOCache<int, int> cache(4);
-	cache.insert(1, 1);
-	cache.insert(2, 2);
-	cache.insert(3, 3);
-	cache.insert(4, 4);
-	std::cout << cache.get(1) << std::endl; // 1
-	std::cout << cache.get(2) << std::endl; // 2
-	std::cout << cache.get(3) << std::endl; // 3
-	std::cout << cache.get(4) << std::endl; // 4
-	cache.insert(5, 5);
-	cache.insert(6, 6);
-	cache.insert(7, 7);
-	cache.insert(8, 8);
-	cache.insert(9, 9);
-	std::cout << cache.get(1) << std::endl; // 0
-	std::cout << cache.get(2) << std::endl; // 2
+	FIFOCache<int, int> cachedt;
+	cachedt.insert(1, 1);
+	std::cout << cachedt.get(1) << std::endl; // 1
 	return 0;
 }
--- CacheAssn.h
+++ CacheAssn.h
@@ -4,27 +4,26 @@
 #include <cstddef>
 #include <unordered_map>
 #include <queue>
+#include <stack>
 
 template<typename K, typename V>
-class FIFOCache {
-	private:
-		size_t max_cache_size;
+class AbstractCache {
+	protected:
 		std::unordered_map<K, V> cache_map;
+		size_t max_cache_size;
+	public:
+		AbstractCache(size_t cache_size);
+		virtual void insert(const K& key, const V& value) = 0;
+		virtual V get(const K& key) = 0;
+};
+
+template<typename K, typename V>
+class FIFOCache : public AbstractCache<K, V> {
+	private:
 		std::queue<K> fifo_map;
 	public:
-		FIFOCache(size_t cache_size = 1024) : max_cache_size(cache_size) {
-			if (cache_size < 8) {
-				std::cout << "Too low cache size. Setting it to 8 elements\n";
-				max_cache_size = 8;
-			}
-			if (cache_size > 65536) {
-				std::cout << "Too large cache size. Setting it to 65536 elements\n";
-				max_cache_size = 65536;
-			}
-		}
-
+		FIFOCache(size_t cache_size = 1024) : AbstractCache<K, V>(cache_size) {}
 		~FIFOCache();
-
 		void insert(const K& key, const V& value);
 		V get(const K& key);
 };
