3ad85b53989d54e5865c0582da0d30793cbedd90
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2024-02-22 07:07:06 +0530

    Add basic FIFO Cache

--- CacheAssn.cpp
+++ CacheAssn.cpp
@@ -1,12 +1,50 @@
-﻿// CacheAssn.cpp : Defines the entry point for the application.
-//
+﻿#include "CacheAssn.h"
 
-#include "CacheAssn.h"
+template<typename K, typename V>
+void FIFOCache<K, V>::insert(const K& key, const V& value) {
+	if (cache_map.size() == max_cache_size) {
+		cache_map.erase(fifo_map.front());
+		fifo_map.pop();
+	}
+	cache_map[key] = value;
+	fifo_map.push(key);
+}
 
-using namespace std;
+template<typename K, typename V>
+V FIFOCache<K, V>::get(const K& key) {
+	if (cache_map.find(key) == cache_map.end()) {
+		std::cerr << "Invalid Key requested. Returning default value\n";
+		return NULL;
+	}
+	return cache_map[key];
+}
+
+template<typename K, typename V>
+FIFOCache<K, V>::~FIFOCache() {
+	while (!fifo_map.empty()) {
+		fifo_map.pop();
+	}
+	cache_map.clear();
+}
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	// Tests for the FIFOCache class
+	FIFOCache<int, int> cache(4);
+	cache.insert(1, 1);
+	cache.insert(2, 2);
+	cache.insert(3, 3);
+	cache.insert(4, 4);
+	std::cout << cache.get(1) << std::endl; // 1
+	std::cout << cache.get(2) << std::endl; // 2
+	std::cout << cache.get(3) << std::endl; // 3
+	std::cout << cache.get(4) << std::endl; // 4
+	cache.insert(5, 5);
+	cache.insert(6, 6);
+	cache.insert(7, 7);
+	cache.insert(8, 8);
+	cache.insert(9, 9);
+	std::cout << cache.get(1) << std::endl; // 0
+	std::cout << cache.get(2) << std::endl; // 2
 	return 0;
 }
--- CacheAssn.h
+++ CacheAssn.h
@@ -1,8 +1,30 @@
-﻿// CacheAssn.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
+#include <cstddef>
+#include <unordered_map>
+#include <queue>
 
-// TODO: Reference additional headers your program requires here.
+template<typename K, typename V>
+class FIFOCache {
+	private:
+		size_t max_cache_size;
+		std::unordered_map<K, V> cache_map;
+		std::queue<K> fifo_map;
+	public:
+		FIFOCache(size_t cache_size = 1024) : max_cache_size(cache_size) {
+			if (cache_size < 8) {
+				std::cout << "Too low cache size. Setting it to 8 elements\n";
+				max_cache_size = 8;
+			}
+			if (cache_size > 65536) {
+				std::cout << "Too large cache size. Setting it to 65536 elements\n";
+				max_cache_size = 65536;
+			}
+		}
+
+		~FIFOCache();
+
+		void insert(const K& key, const V& value);
+		V get(const K& key);
+};
